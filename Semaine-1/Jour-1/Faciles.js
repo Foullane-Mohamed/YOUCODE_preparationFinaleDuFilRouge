c89d526cbeb17489962097b23c2e5bba21152bc0
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2025-03-24 12:29:05 +0000

    ajouter exercice faciles de jour 1 semaine 1

---
+++ Semaine-1/Jour-1/Faciles.js
@@ -0,0 +1,16 @@
+// Déclarez une variable nom avec votre nom 
+// et affichez-la dans la console avec console.log(nom).
+
+let nom = 'mohame'
+console.log(nom)
+
+// Créez une constante age avec votre âge et affichez-la.
+const age = 28
+console.log(age);
+
+// Déclarez une variable estEtudiant avec true si vous êtes étudiant, 
+// sinon false, et affichez-la.
+let etudiant = true;
+let estEtudiant = etudiant?true:false
+console.log(estEtudiant);
+
